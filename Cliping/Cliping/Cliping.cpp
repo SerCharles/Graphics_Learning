3a934cffe69b601b7c304d0b615c0648fe7ffdfd
Charles Shen <ssa19990208@foxmail.com>
2020-09-30 13:08:04 +0800

    交点求解和排序

--- Cliping/Cliping/Cliping.cpp
+++ Cliping/Cliping/Cliping.cpp
@@ -15,7 +15,7 @@ Cliping::Cliping(QWidget *parent)
     ui.setupUi(this);
 	connect(ui.SourceButton, &QPushButton::clicked, this, &Cliping::SwitchToSource);
 	connect(ui.WindowButton, &QPushButton::clicked, this, &Cliping::SwitchToWindow);
-
+	connect(ui.SaveButton, &QPushButton::clicked, this, &Cliping::Weiler_Atherton);
 }
 
 void Cliping::mousePressEvent(QMouseEvent *event)
@@ -113,6 +113,7 @@ void Cliping::CloseLink()
 		}
 		SourcePoints.push_back(BufferPoints);
 		if (out == -1) out = SourcePoints.size() - 1;
+		OutSource = out;
 		//外环按照逆时针，内环顺时针
 		for (int i = 0; i < SourcePoints.size(); i++)
 		{
@@ -150,6 +151,8 @@ void Cliping::CloseLink()
 		}
 		WindowPoints.push_back(BufferPoints);
 		if (out == -1) out = WindowPoints.size() - 1;
+		OutWindow = out;
+
 		//外环按照逆时针，内环顺时针
 		for (int i = 0; i < WindowPoints.size(); i++)
 		{
@@ -241,4 +244,159 @@ void Cliping::SwitchToWindow()
 {
 	mode = WINDOW;
 	BufferPoints.clear();
+}
+
+Point Cliping::GetIntersect(Point p_start, Point p_end, Point q_start, Point q_end)
+{
+	int a_p = p_end.x - p_start.x;
+	int b_p = p_end.y - p_start.y;
+	int c_p = 0 - a_p * p_start.x - b_p * p_start.y;
+
+	int a_q = q_end.x - q_start.x;
+	int b_q = q_end.y - q_start.y;
+	int c_q = 0 - a_q * q_start.x - b_q * q_start.y;
+
+	int det = a_p * b_q - a_q * b_p;
+	if (det == 0)
+	{
+		return Point(-1, -1);
+	}
+
+	int x = (b_q * c_p - b_p * c_q) / det;
+	int y = (a_p * c_q - a_q * c_p) / det;
+
+	if(x > max(p_end.x, p_start.x) || x < min(p_end.x, p_start.x) || x > max(q_end.x, q_start.x) || x < min(q_end.x, q_start.x)) return Point(-1, -1);
+	if(y > max(p_end.y, p_start.y) || y < min(p_end.y, p_start.y) || y > max(q_end.y, q_start.y) || y < min(q_end.y, q_start.y)) return Point(-1, -1);
+
+	Point the_p(x, y);
+	if (det < 0) the_p.enter = 1;
+
+	return the_p;
+
+}
+
+double Cliping::GetK(Point start, Point end, Point middle)
+{
+	if (end.x != start.x)
+	{
+		double d = double(middle.x - start.x);
+		double t = double(end.x - start.x);
+		return d / t;
+	}
+	else
+	{
+		double d = double(middle.y - start.y);
+		double t = double(end.y - start.y);
+		return d / t;
+	}
+}
+
+void Cliping::GetIntersections(Point start_a, Point end_a, int npolygon, int nline, vector<list<Point>>& Window, 
+	vector<vector<vector<Intersection>>>& list_a, vector<vector<vector<Intersection>>>& list_b)
+{
+
+	for (int i = 0; i < Window.size(); i++)
+	{
+		list<Point> window_list = Window[i];
+		auto current = window_list.begin();
+		auto previous = current;
+		int current_num = 0;
+		current++;
+		while (current != window_list.end())
+		{
+			Point start_b = Point(previous->x, previous->y);
+			Point end_b = Point(current->x, current->y);
+			Point intersect = GetIntersect(start_a, end_a, start_b, end_b);
+			if (intersect.x >= 0 && intersect.y >= 0)
+			{
+				double k = GetK(start_a, end_a, intersect);
+				double kk = GetK(start_b, end_b, intersect);
+				Intersection a = Intersection(intersect.x, intersect.y, intersect.enter, k, SOURCE, i, current_num);
+				Intersection b = Intersection(intersect.x, intersect.y, intersect.enter, kk, WINDOW, npolygon, nline);
+				list_a[npolygon][nline].push_back(a);
+				list_b[i][current_num].push_back(b);
+			}
+			current++;
+			previous++;
+			current_num++;
+		}
+		current = WindowPoints[i].begin();
+		{
+			Point start_b = Point(previous->x, previous->y);
+			Point end_b = Point(current->x, current->y);
+			Point intersect = GetIntersect(start_a, end_a, start_b, end_b);
+			if (intersect.x >= 0 && intersect.y >= 0)
+			{
+				double k = GetK(start_a, end_a, intersect);
+				double kk = GetK(start_b, end_b, intersect);
+				Intersection a = Intersection(intersect.x, intersect.y, intersect.enter, k, SOURCE, i, current_num);
+				Intersection b = Intersection(intersect.x, intersect.y, intersect.enter, kk, WINDOW, npolygon, nline);
+				list_a[npolygon][nline].push_back(a);
+				list_b[i][current_num].push_back(b);
+			}
+		}
+	}
+}
+
+void Cliping::SortIntersections(vector<Intersection>& t_list)
+{
+	for (int i = 0; i < t_list.size() - 1; i++)
+	{
+		for (int j = i + 1; j < t_list.size(); j++)
+		{
+			if (t_list[i].k > t_list[j].k)
+			{
+				Intersection temp = t_list[i];
+				t_list[i] = t_list[j];
+				t_list[j] = temp;
+			}
+		}
+	}
+}
+
+void Cliping::Weiler_Atherton()
+{
+	vector<vector<vector<Intersection>>> list_source;
+	vector<vector<vector<Intersection>>> list_window;
+	for (int ii = 0; ii < SourcePoints.size(); ii++)
+	{
+		list<Point> source_list = SourcePoints[ii];
+		vector<list<Point>> window_lists = WindowPoints;
+		auto current = source_list.begin();
+		auto previous = current;
+		current++;
+		int current_num = 0;
+		while (current != source_list.end())
+		{
+			Point start = Point(previous->x, previous->y);
+			Point end = Point(current->x, current->y);
+			GetIntersections(start, end, ii, current_num, window_lists, list_source, list_window);
+			
+
+			current++;
+			previous++;
+			current_num++;
+		}
+		current = WindowPoints[ii].begin();
+		{
+			Point start = Point(previous->x, previous->y);
+			Point end = Point(current->x, current->y);
+			GetIntersections(start, end, ii, current_num, window_lists, list_source, list_window);
+		}
+	}
+	for (int i = 0; i < list_source.size(); i++)
+	{
+		for (int j = 0; j < list_source[i].size(); j++)
+		{
+			SortIntersections(list_source[i][j]);
+		}
+	}
+	for (int i = 0; i < list_window.size(); i++)
+	{
+		for (int j = 0; j < list_window[i].size(); j++)
+		{
+			SortIntersections(list_window[i][j]);
+		}
+	}
+	int kebab = 0;
 }
--- Cliping/Cliping/Cliping.h
+++ Cliping/Cliping/Cliping.h
@@ -14,10 +14,13 @@ struct Point
 {
 	int x;
 	int y;
+	bool enter;
+
 	Point(int tx, int ty)
 	{
 		x = tx;
 		y = ty;
+		enter = 0;
 	}
 
 	double GetDist(Point a)
@@ -25,6 +28,27 @@ struct Point
 		int dx = a.x - x;
 		int dy = a.y - y;
 		return sqrt(dx * dx + dy * dy);
+	}
+};
+
+struct Intersection
+{
+	int x;
+	int y;
+	bool enter;
+	double k;
+	bool type;
+	int num_polygon; //对位
+	int num_line;   //对位
+	Intersection(int tx, int ty, bool tenter, double tk, bool ttype, int npolygon, int nline)
+	{
+		x = tx;
+		y = ty;
+		enter = tenter;
+		k = tk;
+		type = ttype;
+		num_polygon = npolygon;
+		num_line = nline;
 	}
 };
 
@@ -38,13 +62,24 @@ public:
 protected:	//mouse
 	void mousePressEvent(QMouseEvent *event);        //单击
 	vector<list<Point>> SourcePoints;
+	int OutSource = -1;
 	vector<list<Point>> WindowPoints;
+	int OutWindow = -1;
 	list<Point> BufferPoints;
-	
 	//1:默认待裁剪多边形，0：窗口多边形
 	bool mode = 1;
 
-	//闭合图形
+
+	//求交
+	Point GetIntersect(Point p_start, Point p_end, Point q_start, Point q_end);
+	//求交点k
+	double GetK(Point start, Point end, Point middle);
+	//求所有交点
+	void Cliping::GetIntersections(Point start_a, Point end_a, int npolygon, int nline, vector<list<Point>>& Window,
+		vector<vector<vector<Intersection>>>& list_a, vector<vector<vector<Intersection>>>& list_b);	//闭合图形
+	//排序交点
+	void Cliping::SortIntersections(vector<Intersection>& t_list);
+
 	void CloseLink();
 	//判断顺时针
 	bool IsClockWise(list<Point> PointLink);
@@ -53,11 +88,11 @@ protected:	//mouse
 	//画线
 	void paintEvent(QPaintEvent *event);
 	//切换模式
-
 private slots:
 	void Cliping::SwitchToSource();
 	void Cliping::SwitchToWindow();
-
+	//主函数
+	void Cliping::Weiler_Atherton();
 private:
     Ui::ClipingClass ui;
 };
